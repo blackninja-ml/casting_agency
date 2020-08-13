73fe79c1aeb3799c45e93f4304949b3061ba260b
Po Tin Mak <60085635+blackninja-ml@users.noreply.github.com>
2020-08-13 18:37:38 +0800

    Add files via upload

---
+++ models.py
@@ -0,0 +1,90 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_name = "postgres"
+database_path = "postgres://{}@{}/{}".format('postgres:Liszt762!','localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+    return db
+
+def migrations(app):
+    migrate = Migrate(app, db)
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return ({
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender,
+            
+        })
+
+    def __repr__(self):
+        return f'Actor: {self.id}, {self.name}'
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = Column(db.Integer(), primary_key=True)
+    title = Column(db.String(), nullable=False)
+    release_date = Column(db.Date(), nullable=False)
+    genre = Column(db.String(), nullable=False)
+
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return ({
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date.strftime("%m/%d/%y"), 
+            'genre': self.genre,
+        })
+    
+
+  
